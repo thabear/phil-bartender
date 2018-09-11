766370a5299654c4e721def5f90cf3b854edfa79
Aaron Riggs <arsgate@gmail.com>
2018-09-11 18:31:58 -0500

    Update testRunner.py

--- RPI/testRunner.py
+++ RPI/testRunner.py
@@ -1,12 +1,15 @@
 import pump
 from time import sleep
 
-# run for 3 seconds to pour a shot?
-pump.liquor1Run(3)
-
-sleep(10)
+# run for 30 seconds to pour a shot?
+pump.liquor1Run(30)
+pump.liquor2Run(30)
 
 # run soda pump for 5 seconds to pour just shy of a glass?
+pump.sodaValveOpen()
 pump.sodaRun(5)
+pump.sodaValveClose()
+
+sleep(5)
 
 # test results
